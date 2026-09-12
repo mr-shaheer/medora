16f2c22bcbd45a94fc0e064ed76211d0bf02d8a4
Shaheer <mrshaheer.info@gmail.com>
2026-09-12 22:14:51 +0500

    feat: add router metadata

---
+++ app/router/metadata.py
@@ -0,0 +1,5 @@
+MCPS = {
+    "pubmed": "Medical research papers and biomedical literature.",
+    "clinicaltrials": "Clinical trials and study information.",
+    "fda": "FDA drugs, labels, recalls, approvals, and safety information.",
+}
